dad0c8ffe752048aff1f3f95660eab9e8ea57103
unknown <ecof1911@gmail.com>
2022-05-04 12:39:17 +0900

    0.3.3 전체동의와 동의패널들 연결 동의서 컴포넌트로 분리
    함수의 인자로 받아오는 변수들은 string 값으로 받아와 number가 들어가야 하는곳에 들어갈 경우
    오류를 발생하므로 parseInt로 변환해주어야 함

---
+++ src/components/sighup/AgreementComponent.vue
@@ -0,0 +1,111 @@
+<template>
+  <v-container style="width: 450px;">
+    <v-checkbox v-model="allagree" color="orange">
+      <template v-slot:label>
+        <div class="black--text text-subtitle-1">
+          토이쇼핑몰 이용약관, 개인정보 수집 및 이용, 프로모션 정보 수신(선택)에
+          모두 동의합니다.
+        </div>
+      </template>
+    </v-checkbox>
+
+    <v-expansion-panels accordion multiple v-model="open" :readonly="true">
+      <v-expansion-panel v-for="(agree, i) in agreementform" :key="i">
+        <v-expansion-panel-header>
+          <v-checkbox
+            dense
+            hide-details
+            class="pa-0"
+            v-model="agreement[i]"
+            @click="agreemethod(i)"
+            color="orange"
+          >
+            <template v-slot:label>
+              <p v-html="agree.title" class="text-body-1 black--text ma-0"></p>
+            </template>
+          </v-checkbox>
+        </v-expansion-panel-header>
+
+        <v-expansion-panel-content
+          style="max-height: 230px; overflow: auto;"
+          class="text-left"
+        >
+          <p v-html="agree.content" class="text-body-2"></p>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+    <p class="mt-16 text-body-2 red--text" v-if="noagree">토이쇼필몰 이용약관 동의와 <br> 개인정보 수집 및 이용 동의에 동의해주세요.</p>
+
+    <div class="my-16">
+      <v-btn class="mr-3" x-large width="150"
+        @click="$router.push('/')"
+      >취소</v-btn>
+      <v-btn class="ml-3 orange" dark x-large width="150"
+        @click="agreecomplete"
+      >확인</v-btn>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import agreementform from '../../assets/agreement.js'
+
+export default {
+  data() {
+    return {
+      agreementform: agreementform,
+      agreement: {
+        0: false,
+        1: false,
+        2: false,
+      },
+      open: [0, 1, 2],
+      noagree: false
+    }
+  },
+  methods: {
+    // 동의하면 패널 닫기
+    agreemethod(i) {
+      if (this.agreement[i]) {
+        this.open.splice(i, 1, 999)
+      } else {
+        this.open.splice(i, 1, parseInt(i))
+      }
+    },
+
+    agreecomplete() {
+      if (this.agreement[0] && this.agreement[1]) {
+        this.$emit('agreecomplete', this.agreement)
+      } else {
+        this.noagree = true
+      }
+    }
+  },
+  computed: {
+    // 전체 동의와 개별 동의 연결
+    allagree: {
+      get() {
+        let allagree = false
+        if (this.agreement[0] && this.agreement[1] && this.agreement[2]) {
+          allagree = true
+        }
+        return allagree
+      },
+      set(agree) {
+        if (agree) {
+          for (let i in this.agreement) {
+            this.agreement[i] = true
+            this.agreemethod(i)
+          }
+        } else {
+          for (let i in this.agreement) {
+            this.agreement[i] = false
+            this.agreemethod(i)
+          }
+        }
+      },
+    },
+  },
+}
+</script>
--- src/components/sighup/NormalComponent.vue
+++ src/components/sighup/NormalComponent.vue
@@ -1,66 +1,31 @@
 <template>
   <v-container style="width: 450px;">
-    <v-checkbox>
-      <template v-slot:label>
-        <div class="black--text text-subtitle-1">
-          토이쇼핑몰 이용약관, 개인정보 수집 및 이용, 프로모션 정보 수신(선택)에
-          모두 동의합니다.
-        </div>
-      </template>
-    </v-checkbox>
 
-    <v-expansion-panels
-      accordion multiple
-      v-model="open" :readonly="true"
-    >
-      <v-expansion-panel v-for="(agree, i) in agreementform" :key="i">
-        <v-expansion-panel-header class="text-body-1">
-          <v-checkbox
-            dense hide-details
-            class="pa-0" v-model="agreement[i]"
-            @click="agreemethod(i)"
-          >
-            <template v-slot:label>
-              {{ agree.title }}
-            </template>
-          </v-checkbox>
-        </v-expansion-panel-header>
+    <div v-if="nextSighup"></div>
+    <AgreementComponent v-else @agreecomplete="agreeComplete" />
 
-        <v-expansion-panel-content
-          style="max-height: 230px; overflow: auto;"
-          class="text-left"
-        >
-          <p v-html="agree.content" class="text-body-2"></p>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
+
   </v-container>
 </template>
 
 <script>
-import agreementform from '../../assets/agreement.js'
+import AgreementComponent from './AgreementComponent.vue'
+
 export default {
+  components: {
+    AgreementComponent
+  },
   data() {
     return {
-      agreementform: agreementform,
-      agreement: {
-        0 : false,
-        1 : false
-      },
-      open: [0, 1]
+      nextSighup: false,
+      agreement: {},
     }
   },
   methods: {
-    agreemethod(i) {
-      if (this.agreement[i]) {
-        this.open.splice(i, 1, 999)
-      } else {
-        this.open.splice(i, 1, i)
-      }
+    agreeComplete(agreement) {
+      this.nextSighup = true
+      this.agreement = agreement
     }
   },
-  computed: {
-
-  }
 }
 </script>
