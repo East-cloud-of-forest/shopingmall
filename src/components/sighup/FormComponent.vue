3d07bcae053bd0003dc40cad26d2a4aecdbcdfe6
dongun lim <ecof1911@gmail.com>
2022-05-09 23:59:13 +0900

    0.3.7 구글 로그인 버튼 수정, 회원가입시 입력창 검사 버그 해결 >> >> 회원가입시 form에 v-model 해놓은 data를 이용해 마지막 검사 여부를 시행했으나 >> 검사가 시행되기 전에 즉, 아무것도 입력하지 않은 상태에서 ture를 반환하여 >> 전체검사 메소드 자체를 if의 조건으로 집어넣어 해결

--- src/components/sighup/FormComponent.vue
+++ src/components/sighup/FormComponent.vue
@@ -3,9 +3,8 @@
     <v-form
       v-if="nextSighup"
       class="mx-auto"
-      style="width: 400px;"
+      style="width: 400px"
       ref="form"
-      v-model="valid"
       lazy-validation
     >
       <v-subheader>계정 정보</v-subheader>
@@ -112,13 +111,13 @@
     <AgreementComponent
       v-else
       @agreecomplete="agreeComplete"
-      style="width: 460px;"
+      style="width: 460px"
     />
   </v-container>
 </template>
 
 <script>
-import AgreementComponent from './AgreementComponent.vue'
+import AgreementComponent from "./AgreementComponent.vue";
 
 export default {
   components: {
@@ -131,83 +130,82 @@ export default {
       // 날짜 선택 변수
       activePicker: null,
       menu: false,
-      valid: true,
       // 폼 전부 입력시 검사
       formfocus: false,
-      focustarget: '',
+      focustarget: "",
       // 날짜 입력폼 가입하기 눌렀을때만 검사
       daterules: true,
       // 폼 룰
       rules: {
-        required: (value) => !!value || '필수 정보입니다.',
+        required: (value) => !!value || "필수 정보입니다.",
         password: (value) =>
-          this.user.password == value || '입력하신 비밀번호와 다릅니다.',
+          this.user.password == value || "입력하신 비밀번호와 다릅니다.",
         email: (value) => {
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || '이메일 주소를 다시 확인해주세요.'
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "이메일 주소를 다시 확인해주세요.";
         },
       },
       user: {
-        id: '',
-        password: '',
-        name: '',
+        id: "",
+        password: "",
+        name: "",
         date: null,
-        email: '',
-        gender: '',
+        email: "",
+        gender: "",
         agreement: {},
       },
-    }
+    };
   },
   watch: {
     menu(val) {
-      val && setTimeout(() => (this.activePicker = 'YEAR'))
+      val && setTimeout(() => (this.activePicker = "YEAR"));
     },
   },
   methods: {
     // 동의서 작성 완료
     agreeComplete(agreement) {
-      this.nextSighup = true
-      this.user.agreement = agreement
+      this.nextSighup = true;
+      this.user.agreement = agreement;
     },
     // 날짜 고르면 menu를 false 로 만들어줌
     save(date) {
-      this.$refs.menu.save(date)
+      this.$refs.menu.save(date);
     },
     // 폼 전부 입력시 검사
     focus(t) {
-      console.log(t)
-      this.focustarget = t
-      this.formfocus = !this.formfocus
+      console.log(t);
+      this.focustarget = t;
+      this.formfocus = !this.formfocus;
     },
     // 회원가입 완료
     sighup() {
       // 날짜 입력폼 가입하기 눌렀을때만 검사
-      this.daterules = this.rules.required
-      this.$refs.form.validate()
-      if (this.valid) {
-        console.log(this.user)
-        this.$router.push('/welcome')
+      this.daterules = this.rules.required;
+      if (this.$refs.form.validate()) {
+        console.log(this.user);
+        this.$router.push("/welcome");
       } else {
-        console.log('no')
+        console.log("no");
       }
     },
   },
   computed: {
     // 폼 전부 입력시 검사
     focusrulep() {
-      let f = true
-      if (this.focustarget == 'p') {
-        f = this.rules.password
+      let f = true;
+      if (this.focustarget == "p") {
+        f = this.rules.password;
       }
-      return this.formfocus ? true : f
+      return this.formfocus ? true : f;
     },
     focusrulee() {
-      let f = true
-      if (this.focustarget == 'e') {
-        f = this.rules.email
+      let f = true;
+      if (this.focustarget == "e") {
+        f = this.rules.email;
       }
-      return this.formfocus ? true : f
+      return this.formfocus ? true : f;
     },
   },
-}
+};
 </script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,18 +7,18 @@
       class="mx-auto mt-15"
       v-model="loginChoice"
     >
-      <v-btn value="normal">
-        일반회원 로그인
-      </v-btn>
-      <v-btn value="company">
-        판매자회원 로그인
-      </v-btn>
-      <v-btn value="none">
-        비회원 배송조회
-      </v-btn>
+      <v-btn value="normal"> 일반회원 로그인 </v-btn>
+      <v-btn value="company"> 판매자회원 로그인 </v-btn>
+      <v-btn value="none"> 비회원 배송조회 </v-btn>
     </v-btn-toggle>
 
-    <v-card max-width="423" elevation="0" tile outlined class="mx-auto my-12 pa-5">
+    <v-card
+      max-width="423"
+      elevation="0"
+      tile
+      outlined
+      class="mx-auto my-12 pa-5"
+    >
       <LoginCard
         v-if="loginChoice == 'normal' || loginChoice == 'company'"
         :loginchoice="loginChoice"
@@ -30,38 +30,38 @@
         <v-card-subtitle class="orange--text">OR</v-card-subtitle>
         <v-divider></v-divider>
       </div>
-      <v-img
-        :src="require(`../assets/btn_google_signin_light_normal_web@2x.png`)"
-        @click="googleLogin"
-        :style="{ cursor: 'pointer' }"
-        max-width="196"
-        block
-        class="mx-auto"
-      ></v-img>
+      <div class="text-center">
+        <v-btn large color="white" class="my-5" @click="googleLogin">
+          <v-img :src="googlelogo" alt="googlelogo" width="60" />
+          <p class="grey--text text--darken-2 ma-0 subtitle-2 font-weight-medium text-capitalize">
+            Google 계정으로 로그인 하기
+          </p>
+        </v-btn>
+      </div>
     </v-card>
   </v-app>
 </template>
 
 <script>
-import LoginCard from '../components/Login/LoginComponent.vue'
-import NoneCard from '../components/Login/NoneComponenet.vue'
+import LoginCard from "../components/Login/LoginComponent.vue";
+import NoneCard from "../components/Login/NoneComponenet.vue";
+import googlelogo from "@/assets/googleicon.svg";
 
 export default {
   data() {
     return {
-      loginChoice: 'normal',
-    }
+      loginChoice: "normal",
+      googlelogo: googlelogo,
+    };
   },
   components: {
     LoginCard,
     NoneCard,
   },
   methods: {
-    googleLogin() {
-
-    }
+    googleLogin() {},
   },
-}
+};
 </script>
 
 <style>
