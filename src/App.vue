31e7a429ab46cf8a494f83df2931de6d3c28c2ea
unknown <ecof1911@gmail.com>
2022-05-11 16:25:57 +0900

    0.4.1 appbar 네비게이션 추가 메인페이지 캐러셀 추가

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
-  <v-app :style="{maxWidth:routerDynamicSize.bodySize, margin:'auto'}">
-    <v-app-bar id="app-bar" color="white" flat :height="routerDynamicSize.size"
-      :style="{display:routerDynamicSize.size==0?'none':'block'}"
+  <v-app :style="{ maxWidth: routerDynamicSize.bodySize, margin: 'auto' }">
+    <v-app-bar
+      id="app-bar"
+      color="white"
+      flat
+      :height="routerDynamicSize.size"
+      :style="{ display: routerDynamicSize.size == 0 ? 'none' : 'block' }"
     >
-      <div class="d-flex align-center">
-        <LogoComponenet style="width:40px;" class="mx-3" />
-      </div>
+      <v-row>
+        <v-col cols="12" class="d-flex align-center">
+          <div class="d-flex align-center">
+            <LogoComponenet style="width: 40px;" class="mx-3" />
+          </div>
 
-      <v-text-field outlined dense id="serch" hide-details></v-text-field>
+          <v-text-field outlined dense id="serch" hide-details></v-text-field>
 
-      <v-spacer></v-spacer>
-      <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
 
-      <v-btn icon class="mr-2" x-large>
-        <v-icon x-large>mdi-shopping-outline</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        class="mr-2"
-        x-large
-        @click="$router.push('/login').catch(() => {})"
-      >
-        <v-icon x-large>mdi-account-outline</v-icon>
-      </v-btn>
+          <v-btn icon class="mr-2" x-large>
+            <v-icon x-large>mdi-shopping-outline</v-icon>
+          </v-btn>
+          <v-btn
+            icon
+            class="mr-2"
+            x-large
+            @click="$router.push('/login').catch(() => {})"
+          >
+            <v-icon x-large>mdi-account-outline</v-icon>
+          </v-btn>
+        </v-col>
+
+        <v-col
+          cols="12"
+          sm="10"
+          offset="0"
+          offset-sm="1"
+          :style="{ display: routerDynamicSize.size < 120 ? 'none' : 'block' }"
+          class="pa-0 d-flex align-center"
+        >
+          <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn text class="body-2 mr-2" large v-bind="attrs" v-on="on">
+                <v-icon dense class="mr-1">mdi-menu</v-icon>
+                전체상품
+              </v-btn>
+            </template>
+
+            <v-list-item-group class="white">
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-menu</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="'aaa'"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-menu</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="'aaa'"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-menu>
+
+          <span class="body-2 grey--text mr-1">|</span>
+
+          <v-btn
+            text
+            class="body-2 mx-1"
+            large
+            v-for="(n, i) in navMenu"
+            :key="i"
+          >
+            <v-icon dense class="mr-1">{{ n.icon }}</v-icon>
+            {{ n.text }}
+          </v-btn>
+
+          <v-spacer></v-spacer>
+
+          <v-btn
+            text
+            class="body-2"
+            large
+          >
+            <v-icon dense class="mr-1">mdi-calendar-text</v-icon>
+            이벤트
+          </v-btn>
+        </v-col>
+      </v-row>
     </v-app-bar>
 
     <v-main min-height="100%">
@@ -37,37 +106,47 @@ export default {
   name: 'App',
 
   data: () => ({
+    navMenu: [
+      {
+        icon: 'mdi-alarm',
+        text: '타임특가',
+        router: '',
+      },
+      {
+        icon: 'mdi-new-box',
+        text: '신규상품',
+        router: '',
+      }
+    ],
   }),
 
-  methods: {
-
-  },
+  methods: {},
 
   computed: {
     routerDynamicSize() {
-      let size = 120
+      let size = '128'
       let bodySize = '1400px'
-      switch(this.$route.name) {
-        case "login" :
-        case "forgot" :
+      switch (this.$route.name) {
+        case 'login':
+        case 'forgot':
           size = 80
           break
-        case "unmade" :
-        case "sighup" :
-        case "sighupc1" :
-        case "sighupc2" :
-        case "sighupc3" :
+        case 'unmade':
+        case 'sighup':
+        case 'sighupc1':
+        case 'sighupc2':
+        case 'sighupc3':
           size = 0
           bodySize = '100%'
           break
       }
-      return {size:size, bodySize:bodySize}
+      return { size: size, bodySize: bodySize }
     },
   },
 
   components: {
-    LogoComponenet
-  }
+    LogoComponenet,
+  },
 }
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <template>
-  <v-app></v-app>
+  <v-app>
+    <v-row>
+      <v-col cols="12" sm="10" offset="0" offset-sm="1">
+        <v-carousel
+          :show-arrows="false"
+          hide-delimiter-background
+          cycle
+          delimiter-icon="mdi-minus"
+        >
+          <v-carousel-item v-for="a in 5" :key="a">
+            <v-sheet color="orange" height="500">
+              <v-row class="fill-height" align="center" justify="center">
+                <div class="text-h2">{{ a }} Slide</div>
+              </v-row>
+            </v-sheet>
+          </v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+  </v-app>
 </template>
 
 <script>
-  export default {
-    name: 'HomeView',
-  }
+export default {
+  name: 'HomeView',
+}
 </script>
