d4528123400f5d4cb30483927f47ff82fcdc7b47
dongun lim <ecof1911@gmail.com>
2022-05-17 00:29:21 +0900

    0.5.1 상품 상세페이지 api 요청, destroyed로 router 나갈때 감지
    api 요청시 proxy를 통해 요청하기 위해 요청 url 에 localhost:8080 을 추가

--- src/store/index.js
+++ src/store/index.js
@@ -8,18 +8,24 @@ export default new Vuex.Store({
   state: {
     user: [],
     mainproduct: [],
+    currentproduct: {},
+    key: "e7765a8802c849b94cee275f83404522",
   },
   getters: {},
   mutations: {
     unmade() {
       router.push("/unmade");
     },
+    resetcurrentproduct(state) {
+      state.currentproduct = {}
+    }
   },
   actions: {
     getMainToyData({ state }) {
       const xhr = new XMLHttpRequest();
-      const key = "e7765a8802c849b94cee275f83404522";
-      const curl = `openapi/OpenApiService.tmall?key=${key}&apiCode=ProductSearch&keyword=동물인형&targetSearchPrd=KOR&pageSize=12&sortCd=A`
+      const apicode = "ProductSearch";
+      const serchkeyword = "동물인형";
+      const curl = `http://localhost:8080/openapi/OpenApiService.tmall?key=${state.key}&apiCode=${apicode}&keyword=${serchkeyword}&targetSearchPrd=KOR&pageSize=12&sortCd=A`;
 
       xhr.onreadystatechange = function (event) {
         const { target } = event;
@@ -28,18 +34,45 @@ export default new Vuex.Store({
           if (status === 0 || (status >= 200 && status < 400)) {
             let data = xhr.responseXML;
             let product = data.getElementsByTagName("Product");
-            let mainproduct = []
+            let mainproduct = [];
             for (let i of product) {
               mainproduct.push({
-                productId: getProductInnerHTML(i, 'ProductCode'),
-                image: getProductInnerHTML(i, 'ProductImage300'),
-                title: getProductInnerHTML(i, 'ProductName'),
-                lprice: getProductInnerHTML(i, 'ProductPrice'),
-                delivery : getProductInnerHTML(i, 'Delivery')
-              })
+                productId: getProductInnerHTML(i, "ProductCode"),
+                image: getProductInnerHTML(i, "ProductImage300"),
+                title: getProductInnerHTML(i, "ProductName"),
+                lprice: getProductInnerHTML(i, "ProductPrice"),
+                delivery: getProductInnerHTML(i, "Delivery"),
+              });
             }
-            state.mainproduct = mainproduct
+            state.mainproduct = mainproduct;
             console.log(data);
+          } else {
+            console.log(xhr);
+          }
+        }
+      };
+      xhr.open("GET", curl);
+      xhr.send();
+    },
+
+    getCurrentProduct({ state }, productCode) {
+      const xhr = new XMLHttpRequest();
+      const apicode = "ProductInfo";
+      const curl = `http://localhost:8080/openapi/OpenApiService.tmall?key=${state.key}&apiCode=${apicode}&productCode=${productCode}`;
+
+      xhr.onreadystatechange = function (event) {
+        const { target } = event;
+        if (target.readyState === XMLHttpRequest.DONE) {
+          const { status } = target;
+          if (status === 0 || (status >= 200 && status < 400)) {
+            let data = xhr.responseXML;
+            console.log(data);
+            let currentproduct = new Object
+            currentproduct.name = getProductInnerHTML(data, 'ProductName')
+            currentproduct.price = getProductInnerHTML(data, 'Price')
+            currentproduct.img = getProductInnerHTML(data, 'BasicImage')
+            currentproduct.ship = getProductInnerHTML(data, 'ShipFee')
+            state.currentproduct = currentproduct
           } else {
             console.log(xhr);
           }
@@ -53,5 +86,8 @@ export default new Vuex.Store({
 });
 
 function getProductInnerHTML(xml, tagname) {
-  return xml.getElementsByTagName(tagname)[0].innerHTML.replace("<![CDATA[", "").replace("]]>", "")
+  return xml
+    .getElementsByTagName(tagname)[0]
+    .innerHTML.replace("<![CDATA[", "")
+    .replace("]]>", "");
 }
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,18 +1,38 @@
 <template>
-  <v-container>
-    {{id}}
-  </v-container>
+  <div>
+    <v-row class="mt-16">
+      <v-col cols="6">
+        <v-img :src="product.img" width="100%"></v-img>
+      </v-col>
+      <v-col cols="6">
+        <v-card flat>
+          <v-card-title class="pt-0 font-weight-medium">{{
+            product.name
+          }}</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+    <h1>{{ product }}</h1>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      id: this.$route.params.productId
-    }
+
+    };
+  },
+  computed: {
+    product() {
+      return this.$store.state.currentproduct;
+    },
   },
   beforeCreate() {
-
+    this.$store.dispatch("getCurrentProduct", this.$route.params.productId);
+  },
+  destroyed() {
+    this.$store.commit('resetcurrentproduct')
   }
-}
+};
 </script>
